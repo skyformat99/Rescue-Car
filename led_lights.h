f92c38fe4371f000bff6cf36e31f2e6dba03fe6a
durga <gdurga@stanford.edu>
2017-06-04 13:37:57 -0700

    modified .h

---
+++ led_lights.h
@@ -0,0 +1,11 @@
+#ifndef LED_LIGHTS_H
+#define LED_LIGHTS_H
+
+void led_light_init(unsigned int left, unsigned int right);
+void left_signal();
+void right_signal();
+void toggle_leds(unsigned int current_pin, unsigned int prev_pin);
+void dance_pattern();
+void alarm_pattern();
+~                   
+#endif   
