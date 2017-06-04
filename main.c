a319b986b215e9e226901ff7f5bfdf0b8f09be0e
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-06-03 22:53:27 -0700

    merged with ultrasound

--- main.c
+++ main.c
@@ -15,23 +15,44 @@ const unsigned echo = GPIO_PIN2;
 
 static stack[MAX_ELEM];
 static top = -1;
-// data structure:                                                             
+// data structure:
 
-// https://www.tutorialspoint.com/javaexamples/data_stack.htm source referred   
-// http://www.sanfoundry.com/c-program-stack-implementation/                    
+// https://www.tutorialspoint.com/javaexamples/data_stack.htm source referred
+// http://www.sanfoundry.com/c-program-stack-implementation/
 
 /* Should replace it with other things */
 
+
+unsigned get_distance(void) {
+    // write hi for 10usec
+    gpio_write(trigger, 1);
+    delay_us(10);
+    gpio_write(trigger, 0);
+    
+    unsigned start = timer_get_time();
+    delay_us(149); // wait til device settles: 148 = time to go one inch
+    while(!gpio_read(echo))
+        ;
+    
+    unsigned end;
+    while(gpio_read(echo) == 1)
+        ;
+    end = timer_get_time();
+    
+    // ((340M/S / 2) * 39.37inch / meter) / 10^6 = inch/usec
+    return (end - start) / 149;
+}
+
 int isEmpty(){
-  return (top == -1);
+    return (top == -1);
 }
 
 int isFull(){
-  return (top == MAX_ELEM-1);
+    return (top == MAX_ELEM-1);
 }
 
 int push(int i){
-  stack[++top] = i;
+    stack[++top] = i;
 }
 
 int pop(){
@@ -44,45 +65,55 @@ int peek(){
 
 
 void move(int i, int time){
-  if(i==0){
-    left_turn(time);
-  } else if(i==1){
-    reverse_motion();
-    delay_ms(time);
-  } else if(i==2){
-    forward_motion();
-    delay_ms(time);
-  } else if(i==3){
-    right_turn(time);
-  } else {
-    // throw error, should never come here                                  
-  }
+    if(i==0){
+        left_turn(time);
+    } else if(i==1){
+        reverse_motion();
+        delay_ms(time);
+    } else if(i==2){
+        forward_motion();
+        delay_ms(time);
+    } else if(i==3){
+        right_turn(time);
+    } else {
+        // throw error, should never come here
+    }
 }
 
 
 void main(void) {
-  printf_init();
-  timer_init();
-  gpio_init();
-  motor_init();
-  //cir = cir_new(); // initialize circular buffer
-  gpio_set_output(trigger);
-  gpio_set_input(echo);
-  gpio_set_pulldown(echo);
-  delay_ms(40);
-  forward_motion();
-
-  while(1) {
-    unsigned distance = get_distance();
-    printf("distance = %d inches\n", distance);
-    delay_ms(1000);
-    if(distance<5){
-      reverse_motion();
-      delay_ms(2000);
-      right_turn(2000);
-      forward_motion();
+    printf_init();
+    timer_init();
+    gpio_init();
+    motor_init();
+    
+    gpio_set_output(trigger);
+    gpio_set_input(echo);
+    gpio_set_pulldown(echo);
+    delay_ms(40);
+    forward_motion();
+    
+    while(1) {
+        unsigned distance = get_distance();
+        printf("distance = %d inches\n", distance);
+        delay_ms(1000);
+        unsigned left_distance;
+        unsigned right_distance;
+        if(distance<5){
+            reverse_motion();
+            delay_ms(2000);
+            right_turn(2000);
+            right_distance = get_distance();
+            left_turn(2000);
+            left_turn(2000);
+            left_distance = get_distance();
+            if (left_distance < right_distance){
+                turn_right();
+                turn_right();
+            }
+            forward_motion();
+        }
     }
-  }
     int mod = FORWARD;
     int time = 10;
     int input = mod + time * 10;
@@ -108,13 +139,6 @@ void main(void) {
         printf("distance is : %d", num/10);
         printf("distance is : %d", num%10);
     }
-
-  // stop at the very end
+    
+    // stop at the very end
 }
-
-
-// https://www.tutorialspoint.com/javaexamples/data_stack.htm source referred
-// http://www.sanfoundry.com/c-program-stack-implementation/
-
-/* Should replace it with other things */
-
