f8b3c626581f5f1ea7a42cdc8e96530e01d471cf
Kenneth <knturibi@stanford.edu>
2017-06-03 21:55:11 -0700

    Add Audio Sensor

---
+++ audio_sensor.h
@@ -0,0 +1,5 @@
+#include "gpio.h"
+unsigned int audio_pin;
+
+void audio_sensor_init(unsigned int pin);
+int audio_sensor_fetch();
--- main.c
+++ main.c
@@ -1,23 +1,12 @@
-#include "timer.h"
-#include "i2c.h"
-#include "VL6180X.h"
+#include "audio_sensor.h"
+#include "gpio.h"
 #include "printf.h"
-#include "uart.h"
- 
-int  main() {
-  int count = 10;
-printf_init();  
-uart_init();
-  i2c_init();
-  vl6180x_init();
-  printf("I am = %x \n", vl6180x_get_whoami());
-  timer_init();
-  while(count<50){
-    short x,y;
-   vl6180x_read_tof_sensor(&x, &y);
-   printf("sensor measurements: x is%d and y is %y\n",x,y);
-   count++;
-  }
 
-return 0;
-}
+int  main() {
+  int voice_pin = GPIO_PIN24;
+  audio_sensor_init(voice_pin);
+  while (1){
+    int num = audio_sensor_fetch();
+    printf("The value in the %d\n", num);
+  }
+  }
