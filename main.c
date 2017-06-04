1cdb1778dec32c75d39d95d08524fa9e2b7d142b
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-06-03 22:49:57 -0700

    ultra-motor and path merged

--- main.c
+++ main.c
@@ -1,42 +1,71 @@
-
 #include "timer.h"
 #include "gpio.h"
 #include "gpioextra.h"
 #include "printf.h"
 #include "motor.h"
-#include "circular.h"
 
+#define MAX_ELEM 100
+#define LEFTTURN 0
+#define REVERSE 1
+#define FORWARD 2
+#define RIGHTTURN 3
 
 const unsigned trigger = GPIO_PIN3;
 const unsigned echo = GPIO_PIN2;
-cir_t *cir;
 
-unsigned get_distance(void) {
-  // write hi for 10usec
-  gpio_write(trigger, 1);
-  delay_us(10);
-  gpio_write(trigger, 0);
+static stack[MAX_ELEM];
+static top = -1;
+// data structure:                                                             
 
-  unsigned start = timer_get_time();
-  delay_us(149); // wait til device settles: 148 = time to go one inch
-  while(!gpio_read(echo))
-    ;
+// https://www.tutorialspoint.com/javaexamples/data_stack.htm source referred   
+// http://www.sanfoundry.com/c-program-stack-implementation/                    
 
-  unsigned end;
-  while(gpio_read(echo) == 1)
-    ;
-  end = timer_get_time();
+/* Should replace it with other things */
 
-  // ((340M/S / 2) * 39.37inch / meter) / 10^6 = inch/usec
-  return (end - start) / 149;
+int isEmpty(){
+  return (top == -1);
 }
+
+int isFull(){
+  return (top == MAX_ELEM-1);
+}
+
+int push(int i){
+  stack[++top] = i;
+}
+
+int pop(){
+    return stack[top--];
+}
+
+int peek(){
+    return stack[top];
+}
+
+
+void move(int i, int time){
+  if(i==0){
+    left_turn(time);
+  } else if(i==1){
+    reverse_motion();
+    delay_ms(time);
+  } else if(i==2){
+    forward_motion();
+    delay_ms(time);
+  } else if(i==3){
+    right_turn(time);
+  } else {
+    // throw error, should never come here                                  
+  }
+}
+
 
 void main(void) {
   printf_init();
   timer_init();
   gpio_init();
   motor_init();
-  cir = cir_new(); // initialize circular buffer
+  //cir = cir_new(); // initialize circular buffer
   gpio_set_output(trigger);
   gpio_set_input(echo);
   gpio_set_pulldown(echo);
@@ -54,6 +83,38 @@ void main(void) {
       forward_motion();
     }
   }
+    int mod = FORWARD;
+    int time = 10;
+    int input = mod + time * 10;
+    push(input);
+    mod = REVERSE;
+    time = 20;
+    input = mod + time * 10;
+    push(input);
+    mod = LEFTTURN;
+    time = 30;
+    input = mod + time * 10;
+    push(input);
+    mod = RIGHTTURN;
+    time = 40;
+    input = mod + time * 10;
+    push(input);
+    while(!isEmpty()){
+        int num = pop();
+        int mod = num % 10;
+        int time = num / 10;
+        //stop();
+        move(3-mod, time); // reverse direction so 3-mod
+        printf("distance is : %d", num/10);
+        printf("distance is : %d", num%10);
+    }
+
   // stop at the very end
 }
+
+
+// https://www.tutorialspoint.com/javaexamples/data_stack.htm source referred
+// http://www.sanfoundry.com/c-program-stack-implementation/
+
+/* Should replace it with other things */
 
