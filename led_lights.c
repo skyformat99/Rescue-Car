16020501b357e5e3820d20a90405f4efe08878c6
durga <gdurga@stanford.edu>
2017-06-13 16:58:30 -0700

    all

--- led_lights.c
+++ led_lights.c
@@ -15,10 +15,14 @@ void led_lights_init(unsigned int left, unsigned int right, unsigned int back){
 }
 
 void signal(unsigned int pin){
-  gpio_write(pin, ON);
-  delay_ms(1000); //change value
-  gpio_write(pin, OFF);
-  delay_ms(1000); //change value
+    int c = 3;
+  while (c > 0) {  
+    gpio_write(pin, ON);
+    delay_ms(500); //change value
+    gpio_write(pin, OFF);
+    delay_ms(500); //change value
+    c--;
+  }
 }
 
 void signal_left() {
@@ -44,28 +48,3 @@ void alarm_pattern() {
   delay_ms(1000); //change value
 }
 
-//////////////////////////////////////////////////
-/*void dance_pattern(){
-int counter = 0;
-int prev_pin = GPIO_PIN11;
-while(1){
-  int mode_of_num = counter%3;
-  if(mode_of_num == 0){
-    toggle_leds(GPIO_PIN1, prev_pin);
-    prev_pin = GPIO_PIN1;
-  }else if(mode_of_num == 1){
-    toggle_leds(GPIO_PIN10, prev_pin);
-    prev_pin = GPIO_PIN10;
-  }else if(mode_of_num == 2){
-    toggle_leds(GPIO_PIN11, prev_pin);
-    prev_pin = GPIO_PIN11;
-  }
-  printf("Previus pin%d\n",prev_pin );
-counter++;
-}*
-}
-void toggle_leds(unsigned int current_pin, unsigned int prev_pin){
-  gpio_write(current_pin, ON);
-  delay_ms(100);
-  gpio_write(prev_pin, OFF);
-  }*/
--- main.c
+++ main.c
@@ -13,6 +13,21 @@
   static const unsigned trigger = GPIO_PIN3;
   static const unsigned echo = GPIO_PIN2;
   static unsigned int audio_pin = GPIO_PIN5;
+
+ void main4(){
+    led_lights_init(GPIO_PIN7, GPIO_PIN8, GPIO_PIN19);
+  while(1) { 
+   signal_left();
+    delay(1);
+    signal_right();
+    delay(1);
+    signal_back();
+    delay(1);
+    alarm_pattern();
+    delay(1);
+  }
+ }
+
   void main2(){
     gpio_init();
     gpio_set_input(GPIO_PIN26);
@@ -33,29 +48,9 @@
     led_lights_init(GPIO_PIN7, GPIO_PIN8, GPIO_PIN13);
     system_enable_interrupts();
     delay(5);
-    // flashHelp();
-    //test remote control
-    // gpio_set_input(GPIO_PIN21);
-    //  unsigned int x;
-    // while (1){
-    //   x = gpio_read(GPIO_PIN21);
-    //   if(x != 0){
-    //     break;
-    //   } 
-    //   // printf("%d\n",x );
-    //   // delay(0.2);
-    // }
-    
-      //int count = 10;
     int sensitiviy_distance = 15;
     int count = 0;
-    // int timer_get_time()
      while (1) {
-      // if(count == 0){
-      //   display_distance();
-      //   count = 10;
-      // }
-      // count--;
       unsigned distance = get_distance();
       unsigned left_distance;
       unsigned right_distance;
@@ -64,17 +59,17 @@
         delay(1);
         stop();
         display_distance();
+
         right_turn( TURN_TIME);
         stop();
-         display_distance();
+        display_distance();
+
         right_distance = get_distance();
         left_turn(2* TURN_TIME);//to account for two turns
         stop();
-        // left_turn(650);
         display_distance();
+
         left_distance = get_distance();
-        // display_distance();
-        // display_distance();
         if (left_distance <= sensitiviy_distance && right_distance <= sensitiviy_distance){
           flashHelp();
           reverse_motion();
@@ -83,80 +78,7 @@
           right_turn(2* TURN_TIME);
         }
         stop();
-        display_distance();
-        
-        // delay(1);
-        // stop ();
-        // display_distance();
-        // // delay(1);
-        // left_turn(650);
-        // delay(1);
-        // stop();
-        // right_turn(650);
-        // delay(1);
-        // stop();
-        // delay(2);
-
       }
       forward_motion();
       } 
   }
-
-
-  /*#include "timer.h"
-  #include "gpio.h"
-  #include "gpioextra.h"
-  #include "printf.h"
-  #include "motor.h"
-  #include "circular.h"
-
-
-  const unsigned trigger = GPIO_PIN3;
-  const unsigned echo = GPIO_PIN2;
-  cir_t *cir;
-
-  unsigned get_distance(void) {
-    // write hi for 10usec
-    gpio_write(trigger, 1);
-    delay_us(10);
-    gpio_write(trigger, 0);
-
-    unsigned start = timer_get_time();
-    delay_us(149); // wait til device settles: 148 = time to go one inch
-    while(!gpio_read(echo))
-      ;
-
-    unsigned end;
-    while(gpio_read(echo) == 1)
-      ;
-    end = timer_get_time();
-
-    // ((340M/S / 2) * 39.37inch / meter) / 10^6 = inch/usec
-    return (end - start) / 149;
-  }
-
-  void main(void) {
-    printf_init();
-    timer_init();
-    gpio_init();
-    motor_init();
-    cir = cir_new(); // initialize circular buffer
-    gpio_set_output(trigger);
-    gpio_set_input(echo);
-    gpio_set_pulldown(echo);
-    delay_ms(40);
-    forward_motion();
-
-    while(1) {
-      unsigned distance = get_distance();
-      printf("distance = %d inches\n", distance);
-      delay_ms(1000);
-      if(distance<5){
-        reverse_motion();
-        delay_ms(2000);
-        right_turn(2000);
-        forward_motion();
-      }
-    }
-    // stop at the very end
-  }*/
