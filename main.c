18708efa6748a60293b1de4feb5b38d2a0ad1cb0
Kenneth <knturibi@stanford.edu>
2017-06-13 21:35:44 -0700

    finally done

--- main.c
+++ main.c
@@ -1,84 +1,80 @@
-  #include "gpio.h"
-  #include "timer.h"
-  #include "motor.h"
-  #include "printf.h"
-  #include "sensor.h"
-  #include "distance.h"
-  #include "interrupts.h"
-  #include "odometer.h"
-  #include "led_lights.h"
-  #define TURN_TIME 1000
+        #include "gpio.h"
+        #include "timer.h"
+        #include "motor.h"
+        #include "printf.h"
+        #include "sensor.h"
+        #include "distance.h"
+        #include "interrupts.h"
+        #include "odometer.h"
+        #include "led_lights.h"
+        #define TURN_TIME 900
 
-  static int help = 0;
-  static const unsigned trigger = GPIO_PIN3;
-  static const unsigned echo = GPIO_PIN2;
-  static unsigned int audio_pin = GPIO_PIN5;
+        static int help = 0;
+        static const unsigned trigger = GPIO_PIN3;
+        static const unsigned echo = GPIO_PIN2;
+        static unsigned int audio_pin = GPIO_PIN5;
 
- void main3(){
-    led_lights_init(GPIO_PIN7, GPIO_PIN8, GPIO_PIN13);
-  while(1) { 
-   signal_left();
-    delay(1);
-    signal_right();
-    delay(1);
-    signal_back();
-    delay(1);
-    alarm_pattern();
-    delay(1);
-  }
- }
 
-  void main2(){
-    gpio_init();
-    gpio_set_input(GPIO_PIN26);
-    while(gpio_read(GPIO_PIN26)){
+      void main(){
+          gpio_init();
+          timer_init();
+          clock_init();
+          printf_init();
+          motor_init();
+          ultrasonic_init();
+          distance_init();
+          led_lights_init(GPIO_PIN7, GPIO_PIN8, GPIO_PIN13);
+          system_enable_interrupts();
+          //for the remote
+          gpio_set_input(GPIO_PIN16);
+          while(gpio_read(GPIO_PIN16)){
+          }
+          int moving_forward = 1;
+          int sensitiviy_distance = 15;
+          int count = 0;
+          while (1) {
+              unsigned distance = get_distance();
+              unsigned left_distance;
+              unsigned right_distance;
+              //check if the car is within the sensitivity distance of an obstacle
+              if (distance <= sensitiviy_distance) {
+                  reverse_motion();
+                  delay(1);
+                  stop();
+                  display_distance();
 
-    }
-    printf("%s\n","done" );
-  }
+                  right_turn(TURN_TIME);
+                  stop();
+                  display_distance();
+                  right_distance = get_distance();
 
-void main(){
-    gpio_init();
-    timer_init();
-    clock_init();
-    printf_init();
-    motor_init();
-    ultrasonic_init();
-    distance_init();
-    led_lights_init(GPIO_PIN7, GPIO_PIN8, GPIO_PIN13);
-    system_enable_interrupts();
-    delay(5);
-    int sensitiviy_distance = 15;
-    int count = 0;
-    while (1) {
-        unsigned distance = get_distance();
-        unsigned left_distance;
-        unsigned right_distance;
-        if (distance <= sensitiviy_distance) {
-            reverse_motion();
-            delay(1);
-            stop();
-            display_distance();
+                  left_turn(2* TURN_TIME);//to account for two turns
+                  stop();
+                  display_distance();
+                  left_distance = get_distance();
 
-            right_turn(TURN_TIME);
-            stop();
-            display_distance();
-            right_distance = get_distance();
-
-            left_turn(2* TURN_TIME);//to account for two turns
-            stop();
-            display_distance();
-            left_distance = get_distance();
-
-            if (left_distance <= sensitiviy_distance && right_distance <= sensitiviy_distance) {
-                flashHelp();
-                reverse_motion();
-                delay(2);
-            } else if (left_distance < right_distance) {
-                right_turn(2* TURN_TIME);
-            }
-            stop();
-        }
-        forward_motion();
-    } 
-}
+                  //flash the word help if blocked
+                  if (left_distance <= sensitiviy_distance && right_distance <= sensitiviy_distance) {
+                      flashHelp();
+                      reverse_motion();
+                      delay(2);
+                  } else if (left_distance < right_distance) {
+                      right_turn(2* TURN_TIME);
+                  }
+                  stop();
+              }
+              if (moving_forward){
+                forward_motion();
+              }
+               if (!gpio_read(GPIO_PIN16)){
+                stop();
+                if (moving_forward){
+                  moving_forward = 0;
+                  stop();
+                }else{
+                  moving_forward = 1;
+                  forward_motion();
+                }
+               }
+          } 
+      }
